5e52d5ac013dc17a0bebd96edd6e3e894374d5d7
AvT778 <157414934+AvT778@users.noreply.github.com>
2024-03-14 18:08:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-# commenttt
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import requests
+import time
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6812909660:AAEq7qTQTbsuDYYZ2vIKxaEallvQV-2m9YE'
+TEXT1 = 'Все говорят: '
+TEXT2 = ', а ты купи слона!'
+MAX_COUNTER = 100
+offset = -2
+counter = 0
+chat_id: int
+while counter < MAX_COUNTER:
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            UTEXT = result['message'].get('text', ' ')
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT1 + UTEXT + TEXT2}')
+    time.sleep(1)
+    counter += 1
